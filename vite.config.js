e44a7f5b81ef17b229c90a204ac7f7f69f63eb4a
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-02-15 02:36:36 +0100

    dist build

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,30 @@ export default defineConfig({
         name: 'Diamond Collections',
         short_name: 'DC Collections',
         theme_color: '#ffffff',
-        icons: [],
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+        },
+        {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+        },
+        {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+        },
+        {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+        }
+        ],
       }, 
     })
   ],

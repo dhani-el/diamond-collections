547af98984044bd4d030cf38ea602bb6e8c6b308
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-02-15 02:25:50 +0100

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Diamond Collections',
+        short_name: 'DC Collections',
+        theme_color: '#ffffff',
+        icons: [],
+      }, 
+    })
+  ],
+})
